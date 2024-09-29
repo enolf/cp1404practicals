47afc7a08f8f95dff4c0bce0276cdd7347e7c9a4
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-09-29 21:05:43 +1000

    Refactor password_stars.py to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -5,19 +5,22 @@ program to detect password
 
 
 def main():
+    """Convert password into stars based on its length"""
     password = get_password()
 
-    while len(password) < 4:
-        password = input("\nTry again, must be 4 characters: \n")
     print_asterisks(password)
 
 
 def print_asterisks(password):
-    print("Password secured as: ", '*' * len(password))
+    """Print password with stars"""
+    print("password secured as:\n", "*" * len(password))
 
 
 def get_password():
+    """Take a password with a minimum length of 4 characters"""
     password = input("Enter your minimum 4 character long password: \n")
+    while len(password) < 4:
+        password = input("\nTry again, must be 4 characters: \n")
     return password
 
 
