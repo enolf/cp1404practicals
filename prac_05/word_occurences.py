2c26ebe27938c028527244f275dbc22bcc545df4
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-10-18 18:27:46 +1000

    add comments and edit docstring

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,8 +1,12 @@
 """
+CP1404practicals - prac_o5 - Florian N Eisen
+Program analyses user string and counts occurrences of words
+
 Estimated: 20 mins
-Actual:
+Actual: 57 mins
 """
 
+# take string input, separate into individual words and make lower case
 string_user = input("Enter your string you want to analyse:\n").lower().split()
 
 string_lookup = {}  # initialising dictionary used to store string components and properties
@@ -16,5 +20,5 @@ for word in string_user:  # analyse each word in string
     if len(word) > len(longest_word):  # look for longest word
         longest_word = word  # store longest word
 
-for key, count in sorted(string_lookup.items()):  # print the results of analysis
+for key, count in sorted(string_lookup.items()):  # print the results of analysis in alphabetical order
     print(f"{key:{len(longest_word)}} : {count}")
