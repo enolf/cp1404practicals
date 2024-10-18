a09a777e2d5c7fbeb212cb99aefc6e7fce500da4
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-10-18 15:29:50 +1000

    Add dictionary of hex names and corresponding codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,4 @@
+NAMES_TO_HEX_CODES = {"AliceBlue": "#f0f8ff", "British Racing Green": "#004225",
+                      "Cherry Blossom Pink": "#ffb7c5", "Deep Saffron": "#ff9933",
+                      "Vanilla": "#f3e5ab", "Teal": "#008080", "Pistachio": "#93c572",
+                      "Opal": "#a8c3bc", "LimeGreen": "#32cd32", "Key Lime": "#e8f48c"}
