b28ca36efb600daf2a51163cb9c2512b4c5b9c7e
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-10-19 11:29:22 +1000

    Complete emails.py from prac_05

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,3 +1,30 @@
 """
+CP1404 - prac_05 - Florian N Eisen
+Program to store email input and extract user's name
 Estimated: 30 mins
+Actual: 46 mins
 """
+
+
+def main():
+    email_filing_cabinet = {}
+    email_input = input("Email: ")
+    while email_input != "":
+        name_extracted = extract_name(email_input)
+        print(f"Is your name: {name_extracted}? (Y/n)")
+        if input().upper() not in ["Y", ""]:
+            name_extracted = input("Name: ")
+        email_filing_cabinet[email_input] = name_extracted
+        email_input = input("Email: ")
+    for key, value in email_filing_cabinet.items():
+        print(f"{value} ({key})")
+
+
+def extract_name(email_input):
+    name_extracted = email_input.split("@")[0]
+    name_extracted = name_extracted.split(".")
+    name_extracted = " ".join(name_extracted).title()
+    return name_extracted
+
+
+main()
