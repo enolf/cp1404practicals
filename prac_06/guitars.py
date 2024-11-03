2acf0c137095b47cd9d4d4bf63a2769c6493d29e
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-03 23:04:02 +1000

    start guitars.py

---
+++ prac_06/guitars.py
@@ -0,0 +1,16 @@
+"""
+estimated: 32
+actual
+"""
+
+from prac_06.guitar import Guitar
+
+def main():
+
+    guitar_name = input("Name: ")
+    guitar_year =
+    while user_guitar or != "":
+
+
+
+main()
