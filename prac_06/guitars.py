fef80589752decf4b91df414176ef9b849fe19c1
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-03 23:34:01 +1000

    completed requirements for guitars.py by added response of stating all guitars and relaying info via is_vintage method

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,7 +8,7 @@ from prac_06.guitar import Guitar
 
 def main():
 
-    guitar = []
+    guitars = []
     guitar_name = input("Name: ")
 
     while guitar_name != "":
@@ -16,9 +16,21 @@ def main():
         guitar_cost = float(input("Cost: $"))
 
         new_guitar = Guitar(guitar_name, guitar_year, guitar_cost)
-        guitar.append(new_guitar)
+        guitars.append(new_guitar)
         print(f"{new_guitar} added")
         guitar_name = input("Name: ")
+
+        # Guitars to test
+        # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+        # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("These are my guitar")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = " (vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("Where did I leave your guitar?")
 
 
 main()
