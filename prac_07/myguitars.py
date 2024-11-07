b2b711dbefc8a6ea9faab5d3064bf9d1e4d75a4e
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-07 12:21:43 +1000

    scan and extract data from guitars.csv and display via Guitars class

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -3,5 +3,19 @@ CP1404 - prac_07 - Florian N Eisen
 program to use my guitars
 """
 
-import csv
+# import csv
 from prac_07.guitar import Guitar
+
+
+def main():
+    my_guitars = []
+
+    readfile = open(file="guitars.csv", mode='r')
+    for line in readfile:
+        parts= line.split(',')
+        new_guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+        my_guitars.append(new_guitar)
+        print(new_guitar)
+
+
+main()
