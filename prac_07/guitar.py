8b17559086e2c612a59a7c880e61382dd5b4bb9b
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-07 21:55:56 +1000

    Add docstring to methods and remove unused methods from prac_06

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,6 +1,6 @@
 """
-estimate: 20min
-actual: 13 min
+CP1404 - prac_07 - Florian N Eisen
+Guitars class to use as an object in myguitars.py
 """
 
 CURRENT_YEAR = 2024
@@ -8,21 +8,15 @@ CURRENT_YEAR = 2024
 
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
+        """Initialise the class"""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Define how string is printed"""
         return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
 
     def __lt__(self, other):
+        """Define how sorting is performed"""
         return self.year < other.year
-
-    def get_age(self):
-        return CURRENT_YEAR - self.year
-
-    def is_vintage(self):
-        if self.get_age() >= 50:
-            return True
-        else:
-            return False
