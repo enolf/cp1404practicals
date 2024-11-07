432d4f306135fbfb06f36286916c22b972bbb762
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-07 21:08:05 +1000

    Write fn to allow user to add new guitars into program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -10,8 +10,12 @@ FILENAME = "guitars.csv"
 
 
 def main():
+    """Use Guitars class, allow user to add new guitars and sort them by year"""
+
+    # initialise my_guitars list
     my_guitars = []
 
+    # open FILENAME safely and extract data
     with open(file=FILENAME, mode='r') as readfile:
         for line in readfile:
             parts = line.split(',')
@@ -19,7 +23,25 @@ def main():
             my_guitars.append(new_guitar)
             print(new_guitar)
 
-    # print a sorted version based on ascending order of guitars years
+    add_new_guitar(my_guitars )
+
+    sort_by_year(my_guitars)
+
+
+def add_new_guitar(my_guitars ):
+    """Add new guitar to my_guitars"""
+    new_guitar_name = input("Enter new guitar name: ")
+    while new_guitar_name != "":
+        new_guitar_year = int(input("Enter new guitar year: "))
+        new_guitar_cost = float(input("Enter new guitar cost: "))
+        new_guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_cost)
+        my_guitars .append(new_guitar)
+        new_guitar_name = input("Enter new guitar name: ")
+    return my_guitars
+
+
+def sort_by_year(my_guitars):
+    """print a sorted version based on ascending order of guitars years"""
     print("\nHere is a sorted version of the guitars based on the ascending order of years:")
     my_guitars.sort()
     for my_guitar in my_guitars:
