bb4b4a22cf6e3868241b94566e9979286850be68
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-07 20:44:17 +1000

    add sorted listing of guitars. use with to open file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -10,14 +10,18 @@ from prac_07.guitar import Guitar
 def main():
     my_guitars = []
 
-    readfile = open(file="guitars.csv", mode='r')
-    for line in readfile:
-        parts = line.split(',')
-        new_guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
-        my_guitars.append(new_guitar)
-        print(new_guitar)
+    with open(file="guitars.csv", mode='r') as readfile:
+        for line in readfile:
+            parts = line.split(',')
+            new_guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
+            my_guitars.append(new_guitar)
+            print(new_guitar)
 
+    # print a sorted version based on ascending order of guitars years
+    print("\nHere is a sorted version of the guitars based on the ascending order of years")
     my_guitars.sort()
+    for my_guitar in my_guitars:
+        print(my_guitar)
 
 
 main()
