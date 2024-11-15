836a375caf0be227da841b21f3821965e3c8e46f
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-16 01:47:44 +1000

    Add greeting using name_input and ability to clear the input

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -9,7 +9,11 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        print('Greet')
+        name_input = self.root.ids.input_name.text
+        self.root.ids.output_label.text = f'Greetings {name_input}'
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ''
 
 
 BoxLayoutDemo().run()
