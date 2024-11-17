d03f35ef7a5a120d6c3e9121063381a8b76a2765
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-17 15:51:32 +1000

    Add initial parameters to app

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -2,3 +2,13 @@
 CP1404 - prac_08 - Florian N Eisen
 GUI program to convert miles to kilometres
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class ConvertMilesFromKilometres(App):
+
+    def build(self):
+        self.title = 'Convert Miles to Kilometres'
+        self.root = Builder.load_file('convert_miles_kilometres.kv')
