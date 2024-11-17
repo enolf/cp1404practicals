8b4d43acbba0bbbcb6fbf555754badb0e7425f90
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-17 16:34:31 +1000

    Add basic functionality to program including error checking. Need to do SRP

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,6 +6,8 @@ GUI program to convert miles to kilometres
 from kivy.app import App
 from kivy.lang import Builder
 
+KM_TO_MILES = 1.60934
+
 
 class ConvertMilesFromKilometres(App):
 
@@ -13,6 +15,14 @@ class ConvertMilesFromKilometres(App):
         self.title = 'Convert Miles to Kilometres'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_km_input(self):
+        input_km = self.root.ids.input_km.text
+        try:
+            output_miles = float(input_km) * KM_TO_MILES
+            self.root.ids.output_miles.text = str(output_miles)
+        except ValueError or TypeError:
+            pass
 
 
 ConvertMilesFromKilometres().run()
