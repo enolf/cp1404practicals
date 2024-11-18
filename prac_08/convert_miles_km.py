849cc3ad2233a3f23168597e17d7bdcc95b69e6b
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-18 15:08:18 +1000

    Add docstrings to methods

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -10,19 +10,23 @@ KM_TO_MILES = 1.60934
 
 
 class ConvertMilesToKilometres(App):
+    """Main Program - convert miles to kilometres"""
 
     def build(self):
+        """Build the app"""
         self.title = 'Convert Miles to Kilometres'
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def handle_m_input(self):
+        """Handle user input of miles"""
         input_m = self.root.ids.input_m.text
         input_m = self.verify_float(input_m)
         output_miles = float(input_m) * KM_TO_MILES
         self.root.ids.output_km.text = str(output_miles)
 
     def toggle_input(self, amount):
+        """Toggle the user input but +1/-1"""
         input_m = self.root.ids.input_m.text
         input_m = self.verify_float(input_m)
         input_m += amount
@@ -30,6 +34,7 @@ class ConvertMilesToKilometres(App):
         self.handle_m_input()
 
     def verify_float(self, input_m):
+        """Verify the input is a float type"""
         try:
             float(input_m)
             return float(input_m)
