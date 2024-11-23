0f6f893dff28864b8e6496650513f0f86d799573
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-24 09:01:19 +1000

    Add glag_fall class var and docstring to __init__

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -9,6 +9,9 @@ from prac_09.taxi import Taxi
 class SilverServiceTaxi(Taxi):
     """SilverSeriveTaxi is a subclass of Taxi"""
 
+    flag_fall = 4.5  # extra charge
+
     def __init__(self, name, price_per_km, fanciness=0.0):
+        """name, price_per_km, fanciness"""
         super().__init__(name, price_per_km)
         self.price_per_km = Taxi.price_per_km * fanciness
