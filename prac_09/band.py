31846ea03b974b9e6a759382f1950b89c68f743a
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-24 10:48:01 +1000

    Add play method

--- prac_09/band.py
+++ prac_09/band.py
@@ -15,6 +15,13 @@ class Band:
     def __str__(self):
         return f'{self.name} ({[musician.__str__() for musician in self._musicians]})'
 
+    def play(self):
+        for musician in self._musicians:
+            if musician.instruments:
+                print(f'{musician.name} is playing: {musician.instruments}')
+            else:
+                print(f'{musician.name} needs an instrument!')
+        return ''
 
     def add(self, musician):
         self._musicians.append(musician)
