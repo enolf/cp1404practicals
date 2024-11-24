0c33c2100c8a88bf6aa6f36ee0589ce7f1917850
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-24 11:08:00 +1000

    Add docstrings

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,21 +3,24 @@ CP1404 - prac_09 - Florian N Eisen
 Band Class for experimenting with associations
 """
 
-from musician import Musician
-
 
 class Band:
+    """Band class for experimenting with associations between musicians (Musician objects)"""
 
     def __init__(self, name):
+        """Band constructor (name)"""
         self.name = name
         self._musicians = []
 
     def __str__(self):
+        """String of musicians"""
         musicians = ''.join([str(musician) for musician in self._musicians])
         return f'{self.name} ({musicians})'
 
     def play(self):
+        """Print all musician's information in a list"""
         return '\n'.join([musician.play() for musician in self._musicians])
 
     def add(self, musician):
+        """Add a musician to the band"""
         self._musicians.append(musician)
