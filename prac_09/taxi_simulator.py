c7176a217fbfd01afdd6fe53a14a1ef9c3ea52f9
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-24 16:16:54 +1000

    Finish drive and choose taxi methods

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,33 +5,56 @@ MENU = '''q)uit, c)hoose, d)rive'''
 
 
 def main():
-    taxis = [Taxi('VW Kombi', 70), SilverServiceTaxi('TX4', 100, 1.5), SilverServiceTaxi('Bobby Car', 5, 5)]
+    taxis = [
+        Taxi('VW Kombi', 70),
+        SilverServiceTaxi('TX4', 100, 1.5),
+        SilverServiceTaxi('Bobby Car', 5, 5)
+    ]
+    current_taxi = None
+    passenger_bill = 0
 
-    choice = input(f'{MENU}\n').lower()
-    while choice != 'q':
-        if choice == 'c':
-            choose_taxi(taxis)
-        elif choice == 'd':
-            drive_taxi()
+    user_choice = input(f'{MENU}\n').lower()
+    while user_choice != 'q':
+        if user_choice == 'c':
+            current_taxi = choose_taxi(taxis, current_taxi)
+        elif user_choice == 'd':
+            passenger_bill = drive_taxi(current_taxi, passenger_bill)
         else:
             print('Invalid Option')
-        choice = input(f'{MENU}\n').lower()
+        print(f'Here is your bill: ${passenger_bill:.2f}')
+        user_choice = input(f'{MENU}\n').lower()
 
 
-def choose_taxi(taxis):
+def choose_taxi(taxis, current_taxi):
     """Make choice of a Taxi from list"""
     display_taxis(taxis)
+    try:
+        taxi_choice = int(input('Choose Taxi #: '))
+        current_taxi = taxis[taxi_choice-1]
+    except IndexError:
+        print('Option not available')
+    except ValueError:
+        print('Not a number')
+    return current_taxi
 
 
 def display_taxis(taxis):
     """Display the taxis available"""
-    for taxi in taxis:
-        print(f'{taxi}')
+    for taxi in enumerate(taxis, 1):
+        print(f'{taxi[0]}: {taxi[1]}')
 
 
-
-def drive_taxi():
+def drive_taxi(current_taxi, passenger_bill):
     """Drive the selected taxi"""
+    if current_taxi is None:
+        print('Choose a taxi prior to departing')
+    else:
+        drive_distance = int(input('Drive distance: '))
+        current_taxi.start_fare()
+        current_taxi.drive(drive_distance)
+        passenger_bill += current_taxi.get_fare()
+    return passenger_bill
+
 
 
 if __name__ == '__main__':
