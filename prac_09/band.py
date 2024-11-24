1351b6904a606f82cdf0dd61f508e987d89fdf49
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-24 10:42:41 +1000

    Add __str__ to print band

---
+++ prac_09/band.py
@@ -0,0 +1,20 @@
+"""
+CP1404 - prac_09 - Florian N Eisen
+Band Class for experimenting with associations
+"""
+
+from musician import Musician
+
+
+class Band:
+
+    def __init__(self, name):
+        self.name = name
+        self._musicians = []
+
+    def __str__(self):
+        return f'{self.name} ({[musician.__str__() for musician in self._musicians]})'
+
+
+    def add(self, musician):
+        self._musicians.append(musician)
