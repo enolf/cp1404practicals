84996e5dfce1cc18228c9d5cf56df64e7cc00aac
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-24 15:37:23 +1000

    Build methods and write menu sequence

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,7 +1,24 @@
 from prac_09.taxi import Taxi
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
+MENU = '''q)uit, c)hoose, d)rive'''
 
-class TaxiSimulator(Taxi, SilverServiceTaxi):
-    """Simulate a real taxi service"""
-    def __init__(self)
+def main():
+    taxis = [Taxi('VW Kombi', 70), SilverServiceTaxi('TX4', 100, 1.5), SilverServiceTaxi('Bobby Car', 5, 5)]
+
+    choice = input(MENU).lower
+    while choice != 'q':
+        if choice == 'c'
+            choose_taxi(taxis)
+        if choice == 'd':
+            drive_taxi()
+
+def choose_taxi(taxis):
+    """Make choice of a Taxi from list"""
+    print(taxis)
+def drive_taxi():
+    """Drive the selected taxi"""
+
+
+if __name__ == '__main__':
+    main()
