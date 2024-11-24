605da15de68f0b7c57dacd74218a84b4ae10e401
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-24 15:16:50 +1000

    Add taxi_simulator.py

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,7 @@
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+class TaxiSimulator(Taxi, SilverServiceTaxi):
+    """Simulate a real taxi service"""
+    def __init__(self)
