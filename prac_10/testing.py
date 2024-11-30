de33c567967bed8f9bf81c7421168b40d21f816d
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-11-30 15:21:26 +1000

    Fix first TODO

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -1,5 +1,5 @@
 """
-CP1404/CP5632 Practical
+CP1404 - Prac 10 - Florian N Eisen
 Testing code using assert and doctest
 """
 
@@ -9,7 +9,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return ' '.join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -31,9 +31,6 @@ def run_tests():
     assert repeat_string("Python", 1) == "Python"
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
-
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
