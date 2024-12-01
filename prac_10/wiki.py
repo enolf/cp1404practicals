b59ac32677c762408a8f3ef65ab2e7dc4ae84673
Florian N Eisen <florian.eisen@my.jcu.edu.au>
2024-12-01 15:06:19 +1000

    Add return of summary

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -10,14 +10,14 @@ def main():
     entry = input("Enter a Wikipedia Search: ")
     while entry != '':
         try:
-            wiki_result = wikipedia.page(entry, auto_suggest=False)
-            print(wiki_result)
-
+            wiki_result = wikipedia.page(entry, auto_suggest=True, )
+            print(wiki_result.title, '\n', wiki_result.summary, '\n', wiki_result.url)
         except wikipedia.exceptions.DisambiguationError as e:
-            print(e.options)
+            print('We need a more specific title. Try one of the following, or a new search:\n', e.options)
         except wikipedia.exceptions.PageError as e:
             print(e)
         entry = input("Enter a Wikipedia Search: ")
+    print('Thank you!')
 
 
 if __name__ == "__main__":
